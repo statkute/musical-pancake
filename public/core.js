77455b516a27fa207c8eaf8eeb15565196f8edf1
Gintare Statkute <statkute.g@gmail.com>
2018-01-03 14:48:00 +0200

    updates buttons based on JSON data

--- public/core.js
+++ public/core.js
@@ -1,4 +1,5 @@
 var myAppModule = angular.module("myApp", []);
+var allIngredients;
 
 function mainController($scope, $http) {
   $scope.productData = {};
@@ -7,10 +8,9 @@ function mainController($scope, $http) {
 
   $http.get('/api/products')
     .success(function(data) {
-      $scope.first = data['1'];
-      $scope.second = data['2'];
-      $scope.third = data['3'];
+      $scope.products = data
       $scope.productData = data;
+      allIngredients = $scope.products;
       console.log(data['1']);
     })
     .error(function(data) {
@@ -19,7 +19,6 @@ function mainController($scope, $http) {
 
     $scope.chooseIngredient = function(ing) {
       $scope.sendData['choise'] = $scope.productData[ing];
-      console.log($scope.productData[ing]);
       $http.post('/api/products', $scope.sendData)
         .success(function(data) {
           $scope.productData = {};
@@ -41,6 +40,28 @@ function mainController($scope, $http) {
       });
 };
 
-$(document).ready(function(){
+$(document).ready(function() {
+    var temp = 0;
 
+    $(".btn-primary").click(function(){
+        var jsonName = '{{products.';
+        var buttonID = "btn-option-";
+        if (temp < 2){
+            temp++;
+            jsonName += temp;
+            jsonName += ".";
+            for (var i = 1; i < 4; i ++){
+                var tempName = jsonName;
+                var tempId = buttonID;
+
+                tempName += i;
+                tempName += '}}';
+                tempId += i;
+                $(("#"+tempId)).html(allIngredients[temp][i]);
+
+                tempName = jsonName;
+                tempId = buttonID;
+            }
+        }
+    });
 });
