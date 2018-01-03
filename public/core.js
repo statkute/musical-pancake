36d739bc7f917ccd5ed23341b585d6203ed880b4
Gintare Statkute <statkute.g@gmail.com>
2018-01-03 16:40:40 +0200

    half implemented post request

--- public/core.js
+++ public/core.js
@@ -17,18 +17,20 @@ function mainController($scope, $http) {
       console.log('Error: ' + data);
     });
 
-    $scope.chooseIngredient = function(ing) {
-      /*$scope.sendData['choise'] = $scope.productData[ing];
-      $http.post('/api/products', $scope.sendData)
-        .success(function(data) {
-          $scope.productData = {};
-          $scope.product = data;
-          console.log(data);
-        })
-        .error(function(data) {
-          console.log('Error: ' + data);
-      });*/
-    };
+    function postSelections () {
+        $scope.chooseIngredient = function() {
+          $scope.sendData['choise'] = choises;
+          $http.post('/api/products', $scope.sendData)
+            .success(function(data) {
+              $scope.productData = {};
+              $scope.product = data;
+              console.log(data);
+            })
+            .error(function(data) {
+              console.log('Error: ' + data);
+          });
+        };
+    }
 
     /*$http.get('/api/recipe')
       .success(function(data) {
@@ -38,30 +40,64 @@ function mainController($scope, $http) {
       .error(function(data) {
         console.log('Error: ' + data);
     });*/
+
+    $(document).ready(function() {
+
+        var temp = 0;
+
+        $(".btn-primary").click(function(){
+            var buttonID = "btn-option-";
+            if (temp < 2){
+                temp++;
+                for (var i = 1; i < 4; i ++){
+                    var tempId = buttonID;
+                    tempId += i;
+
+                    $(("#"+tempId)).html(allIngredients[temp][i]);
+                    tempId = buttonID;
+                }
+            }
+
+            if (temp == 3){
+                alert(choise1 + " " + choise2 + " " + choise3);
+                temp ++;
+                choises["1"] = choise1;
+                choises["2"] = choise2;
+                choises["3"]= choise3;
+                postSelections.call();
+            }
+
+            if (temp == 2){
+                temp ++;
+            }
+        });
+    });
 };
 
-$(document).ready(function() {
-    var temp = 0;
+var choises = {}
 
-    $(".btn-primary").click(function(){
-        var jsonName = '{{products.';
-        var buttonID = "btn-option-";
-        if (temp < 2){
-            temp++;
-            jsonName += temp;
-            jsonName += ".";
-            for (var i = 1; i < 4; i ++){
-                var tempName = jsonName;
-                var tempId = buttonID;
+var choise1 = "";
+var choise2 = "";
+var choise2 = "";
 
-                tempName += i;
-                tempName += '}}';
-                tempId += i;
-                $(("#"+tempId)).html(allIngredients[temp][i]);
+var totalSelections = 0;
 
-                tempName = jsonName;
-                tempId = buttonID;
-            }
-        }
-    });
-});
+var writeSelection = function(param)
+{
+    switch(totalSelections) {
+        case 0:
+            choise1 = param.innerHTML;
+            totalSelections++;
+            break;
+        case 1:
+            choise2 = param.innerHTML;
+            totalSelections++;
+            break;
+        case 2:
+            choise3 = param.innerHTML;
+            totalSelections++;
+            break;
+        default:
+            break;
+    }
+};
